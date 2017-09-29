1aa1fec0c1611fa23e06de5b545a64afff96ee33
Pokefan10 <32400627+Pokefan10@users.noreply.github.com>
2017-09-29 15:49:03 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args []) {
+        int mice = 2;
+        int meese = 2;
+        System.out.println("Start out with " + mice + " mice and end up with more!");
+        meese = meese *12;
+        System.out.println("Maybe even as many as " + meese + "!");
+        System.out.println("And to think you only started with " + mice + "!");
+    }
+
+}
